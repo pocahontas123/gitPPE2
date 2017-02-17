46ddf8b30781e0ecabfbef13718a1739712ab20b
pocahontas123 <fabienreve@yahoo.fr>
2017-02-17 16:25:51 +0100

    Modification formation.php

--- formation.php
+++ formation.php
@@ -1,7 +1,7 @@
 ﻿<?php
 	session_start();
-	if( !isset( $_SESSION['idEmploye'] ) AND isset ( $_SESSION['mdp'] ) ) {
-		header('Location: connexion.php');
+	if( !isset( $_SESSION['idEmploye'] ) AND !isset ( $_SESSION['mdp'] ) ) {
+		header('Location: login.php');
 	}
 	/*try && catch permettent de gérer les erreurs
 	 On crée une instance (objet $bdd) grace à 'new PDO()'
@@ -10,7 +10,7 @@
 	 le mdp de mysql et son nom: ici root(nom) et rien pour le mdp
 	*/
 	
-	require 'incl/pdo.php';
+	require 'incl/fonctions/pdo.php';
 	
 	/*$req contient le resultat de la méthode de class 'query' de l'objet 'bdd'
 	 le résultat dans $req n'est pas 'utilisable' en l'état
@@ -18,51 +18,52 @@
 	 
 	//récupére le pseudo de la session pour l'afficher à côté de "Vous êtes sur votre..."
 	$varID = $_SESSION['idEmploye'];
-	$req = $bdd->query( "SELECT nom, argent, jours FROM employe WHERE idEmploye = $varID" );
+	$req = $bdd->query( "SELECT nom, creditEmploye, nbJoursEmploye FROM employe WHERE idEmploye = $varID" );
 	//Enregistre dans $data. Cela crée un tableau organisé
 	$data = $req->fetchAll();
 	
-	$req3 = $bdd->query( "SELECT titre, date, duree, contenu, nbJours, lieu, prerequis, credit, prestataire.nom from prestataire, employe join selectionner on employe.idEmploye = selectionner.idEmploye join formation on formation.idFormation = selectionner.idFormation where employe.idEmploye = $varID" );
+	$req3 = $bdd->query( "SELECT formation.idFormation, titre_formation, date_formation, duree_formation, contenu_formation, nbJours_formation, lieu_formation, prerequis_formation, credit_formation, selectionner.etat, prestataire.nomPrestataire from prestataire, employe join selectionner on employe.idEmploye = selectionner.idEmploye join formation on formation.idFormation = selectionner.idFormation where employe.idEmploye = $varID" );
 	$data3 = $req3->fetchAll();
 	
 	//Si j'ai un GET c'est que quelqu'un à cliqué sur un bouton dans la page de mes formations
 	if(extract($_GET)) {
 		
 		//5000 crédits de base
-		$argent = $data[0]['argent'];
+		$creditEmploye = $data[0]['creditEmploye'];
 		//15 jours de base
-		$jours = $data[0]['jours'];
+		$nbJoursEmploye = $data[0]['nbJoursEmploye'];
 		
-		$coutFormation = $data3[0]['credit'];
-		$dureeFormation = $data3[0]['nbJours'];
+		$coutFormation = $data3[0]['credit_formation'];
+		$dureeFormation = $data3[0]['nbJours_formation'];
 		
-		var_dump($argent);
-		var_dump($jours);
+		var_dump($creditEmploye);
+		var_dump($nbJoursEmploye);
 		
 		var_dump($coutFormation);
 		var_dump($dureeFormation);
 		
 		//Si j'ai assé d'argents et de jours par rapport aux prix/nbjours de la formation
-		if($argent >= $coutFormation && $jours >= $dureeFormation) {
+		if($creditEmploye >= $coutFormation && $nbJoursEmploye >= $dureeFormation) {
 			//on récupère ce qui restent pour mettre à jours la BDD
-			$argent = $argent-$coutFormation;
-			$jours = $jours-$dureeFormation;
+			$creditEmploye = $creditEmploye-$coutFormation;
+			$nbJoursEmploye = $nbJoursEmploye-$dureeFormation;
 			
 			//met à jours les jrs et crédit de l'employé
-			$req4 = $bdd->prepare("UPDATE employe SET jours = :nvJours, argent = :nvArgent WHERE idEmploye = $varID");
+			$req4 = $bdd->prepare("UPDATE employe SET nbJoursEmploye = :nvJours, creditEmploye = :nvArgent WHERE idEmploye = $varID");
 			$req4->execute(array(
-				'nvJours' => $jours,
-				'nvArgent' => $argent,
+				'nvJours' => $nbJoursEmploye,
+				'nvArgent' => $creditEmploye,
 			));
 			
 			//Rajoute la formation de l'employé
-			$req2 = $bdd->prepare( 'INSERT INTO selectionner (idEmploye, idFormation) VALUES (:idEmploye, :idFormation)' );
+			$req2 = $bdd->prepare( 'INSERT INTO selectionner (idEmploye, idFormation, etat) VALUES (:idEmploye, :idFormation, :etat)' );
 			$req2->execute(array(
 				'idEmploye' => $varID,
 				'idFormation' => $idFormation,
+				'etat' => "En cours de validation"
 			));
 		}else {
-			echo 'manque argent ou jours';
+			echo 'manque crédits ou jours';
 		}
 	}
 ?>
@@ -91,40 +92,51 @@
 	</head>
 	<body>
 		
-		<?php include("incl/menu.php"); ?>
-		<?php include("incl/menu2.php"); ?>
-		<div class="container-fluid">
-			<h1 style="text-align: center;">Vous étes sur votre compte: <strong><?= $data[0]['nom']; ?></strong></h1>
-			<h1 style="text-align: center;">Argents: <strong><?= $data[0]['argent']; ?></strong></h1>
-			<h1 style="text-align: center;">Jours: <strong><?= $data[0]['jours']; ?></strong></h1>
-		</div>
+		<?php include("incl/menus/menu.php"); ?>
+		<?php include("incl/menus/menu2.php"); ?>
+		<?php include("incl/menus/formationMenuInformation.php"); ?>
+
 		
 		<h1>Formations inscrites: </h1>
 		<!-- On affiche le contenu de $data -->
 		<div id="formationsInscription">
 			<?php foreach($data3 as $key => $formations) :?>
 			
-				</br></br></br>
-				<!-- Le titre de l'article en titre -->
-				<h3 style="font-size:30px;"><u><?= $formations['titre'] ;?></u></h3>
-				</br>
-				<p><strong>Contenu:</strong> </p><?= $formations['contenu'] ;?>
-				</br></br>
-				<p><strong>Date:</strong> <?= $formations['date'] ;?></p>
-				</br>
-				<p><strong>Durée(h):</strong> <?= $formations['duree'] ;?> h </p>
-				</br/>
-				<p><strong>Nb Jours:</strong> <?= $formations['nbJours'] ;?> j</p>	
-				</br>
-				<p><strong>Lieu :</strong> <?= $formations['lieu'] ;?> </p>	
-				</br>
-				<p><strong>Prerequis :</strong> <?= $formations['prerequis'] ;?> </p>	
-				</br>
-				<p><strong>Prestataire:</strong> <?= $formations['nom'] ;?></p>
+				<table class="table table-hover">
+					<thead>
+						<tr>
+							<th>Titre</th>
+							<th>Contenu</th>
+							<th>Date</th>
+							<th>Duree</th>
+							<th>Nb Jours</th>
+							<th>Lieu</th>
+							<th>Prerequis</th>
+							<th>Prestataire</th>
+							<th>Etat</th>
+							<th>PDF</th>
+							<th>Désinscrire</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td><strong><?= $formations['titre_formation']; ?></strong></td>
+							<td><?= $formations['contenu_formation'] ;?></td>
+							<td><?= $formations['date_formation']; ?></td>
+							<td><?= $formations['duree_formation']; ?></td>
+							<td><?= $formations['nbJours_formation']; ?></td>
+							<td><?= $formations['lieu_formation']; ?></td>
+							<td><?= $formations['prerequis_formation']; ?></td>
+							<td><?= $formations['nomPrestataire']; ?></td>	
+							<td><?= $formations['etat']; ?></td>	
+							<td><i><button class="btn btn-info">Convertir pdf</button></i></td>
+							<td><a href="delete.php?idFormation=<?= $formations['idFormation'] ;?>"><i><button class="btn btn-primary">Se désinscrire</button></i></a></td>
+						</tr>
+					</tbody>
+				</table>						
 
-				
 			<?php endforeach;?>
-		</div>	
+		</div>		
 		<script src="js/javascript.js"></script>
 	</body>
 </html>
